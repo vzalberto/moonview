4ea13e94b2c16934dbf0c63d12ef8244b8caf9d6
Alberto <vzalberto@msn.com>
2019-06-04 05:09:46 -0500

    First

---
+++ moonfront.js
@@ -0,0 +1,16 @@
+// create illo
+let illo = new Zdog.Illustration({
+  // set canvas with selector
+  element: '.zdog-canvas',
+});
+
+// add circle
+new Zdog.Ellipse({
+  addTo: illo,
+  diameter: 80,
+  stroke: 20,
+  color: '#636',
+});
+
+// update & render
+illo.updateRenderGraph();
